425c9517c1926dd833331f94abbdb5cffe699a11
KAMLESH KUMAR MAHAWAR <kamleshkumar.mahawar7@gmail.com>
2019-10-29 11:20:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export class App extends Component {
     this.state = {
       showLoader: false,
       currentView: 'future',
-
+      showDateSelector: {},
     };
   }
 
@@ -36,23 +36,32 @@ export class App extends Component {
 
   changeDateHandler(e, section) {
     this.props.dispatchUpdate(e.target.value, section);
+    this.setState({ showDateSelector: '' });
   }
 
+  toggleDate(e, section) {
+    e.stopPropagation();
+    this.setState({ showDateSelector: section.eventName });
+  }
+
+  hideDate() {
+    this.setState({ showDateSelector: '' });
+  }
 
   render() {
     const { sections } = this.props;
-    const { showLoader, currentView } = this.state;
+    const { showLoader, currentView, showDateSelector } = this.state;
     return (
-      <div className="App">
+      <div className="App" onClick={this.hideDate.bind(this)}>
         <h1>Manage Campaigns</h1>
         <hr />
         {showLoader && <div className="loader">
           <img src="./loader.gif" alt="Loading..." width="400" height="400" />
         </div>}
         <div className="tab-btn-wrapper">
-          <button className={currentView === 'future' && 'active'} onClick={this.changeView.bind(this, 'future')}>UPCOMING COMPAIGNS</button>
-          <button className={currentView === 'live' && 'active'} onClick={this.changeView.bind(this, 'live')}>LIVE COMPAIGNS</button>
-          <button className={currentView === 'past' && 'active'} onClick={this.changeView.bind(this, 'past')}>PAST COMPAIGNS</button>
+          <button className={currentView === 'future' ? 'active' : ''} onClick={this.changeView.bind(this, 'future')}>UPCOMING COMPAIGNS</button>
+          <button className={currentView === 'live' ? 'active' : ''} onClick={this.changeView.bind(this, 'live')}>LIVE COMPAIGNS</button>
+          <button className={currentView === 'past' ? 'active' : ''} onClick={this.changeView.bind(this, 'past')}>PAST COMPAIGNS</button>
         </div>
 
         <table className="table">
@@ -68,7 +77,7 @@ export class App extends Component {
             {
               sections && sections[currentView] && sections[currentView].length > 0 &&
               sections[currentView].map((section) => (
-                <tr>
+                <tr key={section.eventName}>
                   <td>
                     <div>{section.date}</div>
                     <div>{`${section.days === 0 ? '' : section.days} ${labels[currentView]}`}</div>
@@ -90,8 +99,11 @@ export class App extends Component {
                     </div>
                   </td>
                   <td>
-                    <div className="center cursor action">
-                      <input type="date" className="date" value={section.date} onChange={(e) => this.changeDateHandler(e, section)} />
+                    <div className="center cursor action" onClick={(e) => this.toggleDate(e, section)} >
+                      <img src="./Calendar.png" alt="loading..." />
+                      {(showDateSelector === section.eventName) && <div className="dateSelector">
+                        <input type="date" className="date" value={section.date} onChange={(e) => this.changeDateHandler(e, section)} />
+                      </div>}
                     </div>
                   </td>
                 </tr>
@@ -116,6 +128,3 @@ const mapDispatchToProps = {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-
-
