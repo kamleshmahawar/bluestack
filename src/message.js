b2a8855a768aaecda3d88592c667953d9cb019ba
KAMLESH KUMAR MAHAWAR <kamleshkumar.mahawar7@gmail.com>
2019-10-29 12:25:31 +0530

    Add files via upload

---
+++ src/message.js
@@ -0,0 +1,17 @@
+export const EN = {
+  'Test Whatsapp': 'Test Whatsapp',
+  'Test nokia': 'Test nokia',
+  'Super Jewels Quest': 'Super Jewels Quest',
+  'Mole Slayer': 'Mole Slayer',
+  'Mancala Mix': 'Mancala Mix',
+  'Test Twitter': 'Test Twitter',
+}
+
+export const JP = {
+  'Test Whatsapp': 'Whatsappをテストする',
+  'Test nokia': 'テストノキア',
+  'Super Jewels Quest': 'スーパージュエルズクエスト',
+  'Mole Slayer': 'ほくろスレイヤー',
+  'Mancala Mix': 'マンカラミックス',
+  'Test Twitter': 'Twitterをテストする',
+}
